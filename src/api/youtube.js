6eeda6515a1625eb704f7219e4b937de8ef84006
ryanrobinson1 <45307703+ryanrobinson1@users.noreply.github.com>
2019-02-09 12:09:01 +0000

    removed api key

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,5 @@
 import Axios from "axios";
 
-const KEY = "AIzaSyD92E-273REX3w-oTK76J3WNKeFLp62F-g";
-
 const youtubeApi = Axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   timeout: 10000,
